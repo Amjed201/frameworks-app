a05e9b782639ea8b875a4f7811d478533c289862
amjed201 <amjed4ever@hotmail.com>
2025-07-03 03:07:36 +0300

    Initial Commit

---
+++ frameworks-app/frameworks_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  frameworks_appApp.swift
+//  frameworks-app
+//
+//  Created by Amjed Mohamed Babiker on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct frameworks_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
